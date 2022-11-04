ad8a5f82f5fb1719c42c2e35a254ab073c920f12
Urano76 <116237385+Urano76@users.noreply.github.com>
2022-11-04 16:44:41 -0600

    Add files via upload

---
+++ tres.js
@@ -0,0 +1,13 @@
+/*Esta tabla se hace para combinar dos*/
+const cal = () => {
+    const calificaciones = {
+        Juan: 10,
+        Jose: 8,
+        Luis: 6.5,
+        Alvaro: 9,
+        Geronimo: 8.5
+
+    }
+    return calificaciones;
+}
+console.table(cal());
