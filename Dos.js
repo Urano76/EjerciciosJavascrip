4d868b2c49fb5103ff3308110944c19e56514f56
Urano76 <116237385+Urano76@users.noreply.github.com>
2022-11-04 13:45:15 -0600

    Add files via upload

---
+++ Dos.js
@@ -0,0 +1,11 @@
+let alumnos = ['Juan', 'Pedro', 'Luis', 'Geronimo', 'Julian', 'Diego', 'Jose', 'Jesus', 'Felipe'];
+
+/*alumnos.forEach(function(disipulos) {
+    console.log(disipulos)
+});*/
+
+alumnos.forEach((disipulos) => {
+
+    console.table(disipulos)
+
+});
