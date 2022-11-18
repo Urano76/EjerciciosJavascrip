f6a709a69c70451ef6fa990c45b9865d3b64ae38
Urano76 <116237385+Urano76@users.noreply.github.com>
2022-11-18 16:34:49 -0600

    Add files via upload

---
+++ Ejercicios12.js
@@ -0,0 +1,17 @@
+/*Ejercicio Bidimencional*/
+
+let result = [{ alumno: 'Pedro', calificacion: 8 }, { alumno: 'Luis', calificacion: 9 }, { alumno: 'Pablo', calificacion: 7 }, { alumno: 'Sergio', calificacion: 10 }, { alumno: 'Antonio', calificacion: 8.5 }];
+console.table(result);
+
+
+/*Ejercicio con while*/
+
+const dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+let a = 0;
+let semana = " ";
+while (a < dias.length) {
+
+    semana = dias;
+    a++;
+}
+console.table(semana);
