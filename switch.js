634b408489730d4b0af76cf0dd9816eeeeeeef16
Urano76 <116237385+Urano76@users.noreply.github.com>
2022-12-08 23:28:47 -0600

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,24 @@
+ const calificacion = 'A';
+
+ switch (calificacion) {
+     case 'A':
+         console.log('Eres un excelente alumno');
+         break;
+     case 'B':
+         console.log('Eres un muy buen alumno');
+         break;
+     case 'C':
+         console.log('Eres un buen alumno');
+     case 'D':
+         console.log('Eres un regular como alumno');
+         break;
+     case 'E':
+         console.log('Eres un mediocre como alumno');
+         break;
+     case 'F':
+         console.log('Eres un mal alumno');
+         break;
+     default:
+         console.log(calificacion, 'Se termino el ejercio');
+
+ }
