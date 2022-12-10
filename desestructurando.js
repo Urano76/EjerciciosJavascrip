238c8a6577d585d646467489cac6450c76d6dd3d
Urano76 <116237385+Urano76@users.noreply.github.com>
2022-12-10 01:39:11 -0600

    Add files via upload

---
+++ desestructurando.js
@@ -0,0 +1,17 @@
+const barco = {
+    color: 'estampado',
+    toneladas: 200000,
+    ano: 2018,
+    tipo: 'crucero',
+}
+
+// console.table(barco);
+
+const { toneladas } = barco;
+console.log(toneladas);
+
+// const { tipo, ano } = barco;
+// console.log(tipo, ano);
+
+// const { toneladas, color } = barco;
+// console.log(toneladas, color);
