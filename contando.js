97c07be1e1196b3215146f40133d0cb042d309b8
Urano76 <116237385+Urano76@users.noreply.github.com>
2023-03-14 18:51:21 -0600

    Add files via upload

--- contando.js
+++ contando.js
@@ -1,8 +1,22 @@
-function palabra (a = ''){
-    if (typeof a !='string')
-    return`Ingresa palabra o frase`;
-    if(a === '') 
-    return`No has ingresado nada`;
-    return a.length;
-}
-console.log(palabra('alto'));
+// function palabra (a = ''){
+//     if (typeof a !='string')
+//     return`Ingresa palabra o frase`;
+//     if(a === '') 
+//     return`No has ingresado nada`;
+//     return a.length;
+// }
+// console.log(palabra('alto'));
+const lista = new Promise ((resolve,reject) =>{
+    function palabra (a = ''){
+            if (typeof a !='string'){
+              resolve(`Ingresa una palabra o frase`);  
+            }
+            else if(a === '') {
+            reject(`No ha ingresado nada`);    
+            }
+            return a.length;
+        }
+        console.log(palabra('Bienvenidos a Javascrip'));
+    
+})
+lista.then(funciona => console.log(funciona)).catch(Sinfuncionar => console.log(Sinfuncionar))
