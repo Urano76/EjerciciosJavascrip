1a9ad570de0c870b350ee6d07265f11097f6d289
Urano76 <116237385+Urano76@users.noreply.github.com>
2023-03-21 18:57:59 -0600

    Add files via upload

---
+++ lugar.js
@@ -0,0 +1,6 @@
+const cuatro = new Promise((resolve,reject) => {
+let h = ['Javier','Tomas','Luis','Santiago'];
+    const alumno ='Luis';
+       
+});
+cuatro.then(aceptado => console.log(aceptado)).catch(rechazado => console.log(rechazado))
